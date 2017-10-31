e90048e80496b2a561f0936c0d69ae81808ef88b
matsuhisa <matsuhisa+github@gmail.com>
2017-10-31 16:25:09 +0900

    Add bot.rb

---
+++ lib/bot.rb
@@ -0,0 +1,21 @@
+require 'dotenv/load'
+require 'slack-ruby-client'
+
+Slack.configure do |conf|
+  conf.token = ENV['API_TOKEN']
+end
+
+client = Slack::RealTime::Client.new
+
+client.on :hello do
+  puts 'connected!'
+end
+
+client.on :message do |data|
+  case data['text']
+  when 'にゃーん' then
+    client.message channel: data['channel'], text: ':cat:'
+  end
+end
+
+client.start!
